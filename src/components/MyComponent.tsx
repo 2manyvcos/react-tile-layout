5cd861bae60260b3917c3477bf24b3552783d67c
Aaron Burmeister <aaron.burmeister@ils-gmbh.net>
2021-04-16 15:42:24 +0200

    Implementation

--- src/components/MyComponent.tsx
+++
@@ -1,9 +0,0 @@
-interface MyComponentProps {
-  name: string;
-}
-
-const MyComponent: React.FC<MyComponentProps> = ({ name }) => {
-  return <div>{name}</div>;
-};
-
-export default MyComponent;
---
+++ src/components/types.ts
@@ -0,0 +1,36 @@
+export namespace ReactTileLayout {
+  export type Dictionary<K extends keyof any, T> = Partial<Record<K, T>>;
+
+  export type Breakpoints = { [P: string]: number };
+
+  export type TileLayout = {
+    [Key in keyof ReactGridLayout.Layout as Exclude<
+      Key,
+      "i" | "x" | "y"
+    >]: ReactGridLayout.Layout[Key];
+  } & {
+    override?: number | string;
+  };
+
+  export interface TileDescription {
+    layout: TileLayout;
+    disabled: boolean;
+  }
+
+  export type TileDefinitionProps = Dictionary<string, any>;
+
+  export interface TileDefinition extends TileDescription {
+    props: TileDefinitionProps;
+  }
+
+  export type NullableTileDefinition = TileDefinition | undefined;
+
+  export type TileValueOrResolver =
+    | NullableTileDefinition
+    | ((previous: NullableTileDefinition) => NullableTileDefinition);
+
+  export type TileComponentProps = TileDefinitionProps & {
+    name: string;
+    layoutSpec: TileLayout;
+  };
+}
